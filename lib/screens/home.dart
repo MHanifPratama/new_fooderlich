79579ac09d30cd30d63c67f28dd0715fdf7903bd
MHanifPratama <mhanifp27@gmail.com>
2022-11-01 21:27:35 +0700

    NewFooderlich

---
+++ lib/screens/home.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'explore_screen.dart';
+import 'grocery_screen.dart';
+import 'recipes_screen.dart';
+
+import 'package:provider/provider.dart';
+import '../models/models.dart';
+
+class Home extends StatefulWidget {
+  const Home({
+    super.key,
+    required this.currentTab,
+  });
+
+  final int currentTab;
+
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static const String prefSelectedIndexKey = 'selectedTab';
+  static List<Widget> pages = <Widget>[
+    ExploreScreen(),
+    RecipesScreen(),
+    const GroceryScreen(),
+  ];
+  @override
+  void initState() {
+    super.initState();
+    getCurrentIndex();
+  }
+
+  void getCurrentIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey(prefSelectedIndexKey)) {
+      setState(() {
+        final index = prefs.getInt(prefSelectedIndexKey);
+        if (index != null) {
+          _selectedIndex = index;
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        actions: [
+          profileButton(widget.currentTab),
+        ],
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // currentIndex: widget.currentTab,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          saveCurrentIndex();
+
+          // Provider.of<AppStateManager>(context, listen: false)
+          // .goToTab(index);
+          // context.goNamed(
+          //   'home',
+          //   params: {
+          //     'tab': '$index',
+          //   },
+          // );
+          // saveCurrentIndex();
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Recipes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'To Buy',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget profileButton(int currentTab) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: GestureDetector(
+        child: const CircleAvatar(
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage(
+            'assets/profile_pics/person_stef.jpeg',
+          ),
+        ),
+        onTap: () {
+          context.goNamed('profile', params: {
+            'tab': '$currentTab',
+          });
+        },
+      ),
+    );
+  }
+
+  void saveCurrentIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt(prefSelectedIndexKey, _selectedIndex);
+  }
+}
