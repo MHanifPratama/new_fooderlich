736f6f9fc543a032f66e34c84ec23f6b589ad484
MHanifPratama <mhanifp27@gmail.com>
2022-11-24 13:40:57 +0700

    dio

--- lib/api/get_recipe.dart
+++ lib/api/get_recipe.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
+import 'package:dio/dio.dart';
 import '../models/explore_recipe.dart';
 // import 'package:flutter/services.dart';
 import '../models/models.dart';
@@ -8,11 +8,12 @@ import '../models/models.dart';
 class GetRecipe {
   static var URL = 'https://uji.fmipa.unila.ac.id/api-test/recipes.json';
   Future<List<SimpleRecipe>> getRecipes() async {
-    var response = await http
-        .get(Uri.parse('https://uji.fmipa.unila.ac.id/api-test/recipes.json'));
+    // var response = await http.get(Uri.parse('https://uji.fmipa.unila.ac.id/api-test/recipes.json'));
+    var response =
+        await Dio().get('https://uji.fmipa.unila.ac.id/api-test/recipes.json');
     if (response.statusCode == 200) {
       // var data = json.decode(response.body);
-      final Map<String, dynamic> json = jsonDecode(response.body);
+      final Map<String, dynamic> json = response.data;
       // Go through each recipe and convert json to SimpleRecipe object.
       if (json['recipes'] != null) {
         final recipes = <SimpleRecipe>[];
